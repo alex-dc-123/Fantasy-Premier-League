64390832ca01eb2dcb0489b3f52f9d21ad997102
alexnader.drummond-chew <alexander.drummond-chew@oddschecker.com>
2020-10-29 14:32:14 +0000

    first commit

---
+++ DC_edit/fpl_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+# import os 
+
+# os.chdir('')
+season = pd.read_csv('C:/Users/adr01/OneDrive/documents/Python Projects/fpl/Fantasy-Premier-League/data/2020-21/cleaned_players.csv')
+print(season.head())
